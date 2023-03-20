7902dc1576ffa7d135dc15fa0a74f45a43cf85e0
Ahmad Al Shihabi <ahmadalshihabi99@gmail.com>
2023-03-20 14:23:03 +0100

    Remove Button added

--- src/main/java/se/ch/HAnS/fileAnnotation/MappingService.java
+++ src/main/java/se/ch/HAnS/fileAnnotation/MappingService.java
@@ -1,29 +1,16 @@
 package se.ch.HAnS.fileAnnotation;
 
 import com.intellij.notification.NotificationType;
-import com.intellij.openapi.Disposable;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
-import com.intellij.openapi.ui.Messages;
-import com.intellij.openapi.util.Computable;
-import com.intellij.openapi.util.Key;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.*;
 import com.intellij.psi.search.FilenameIndex;
 import com.intellij.psi.search.GlobalSearchScope;
-import com.intellij.psi.util.PsiModificationTracker;
-import com.intellij.psi.util.PsiUtilCore;
-import com.intellij.ui.EditorNotifications;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import java.util.Collection;
 import java.util.Iterator;
-import java.util.Optional;
-import com.intellij.util.containers.Stack;
-import org.jetbrains.annotations.NotNull;
 import com.intellij.notification.Notification;
-import com.intellij.notification.NotificationType;
 import com.intellij.notification.Notifications;
 
 public class MappingService {
